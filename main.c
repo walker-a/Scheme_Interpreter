3f3b5c064a21ab2496ce3a6f69ffef0f4ec011fb
walkera2 <walkera2@cmc306-07.mathcs.carleton.edu>
2017-05-11 18:40:41 -0500

    added more files

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "tokenizer.h"
+#include "value.h"
+#include "linkedlist.h"
+#include "parser.h"
+#include "talloc.h"
+#include "interpreter.h"
+
+int main() {
+
+    Value *list = tokenize(stdin);
+    Value *tree = parse(list);
+    printTree(tree);
+    printf("\n");
+    //interpret(tree);
+
+    tfree();
+    return 0;
+}
