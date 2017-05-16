9d95d6bb625539222a1bffa5ced06247e9f52aad
Alex Walker <alexwalker@eduroam-161-93.dyn.carleton.edu>
2017-05-15 19:20:43 -0500

    undo previous thing that was bad

--- interpreter.c
+++ interpreter.c
@@ -150,9 +150,6 @@ Value *evalLet(Value *expr, Frame *frame) {
         }
         
         Value *symbol = car(assign);
-        if (car(cdr(assign))->type == CONS_TYPE) {
-            handleInterpError();
-        }
         Value *result = car(cdr(assign));
         newFrame->bindings = addBinding(symbol, result, newFrame->bindings);
         assignList = cdr(assignList);
